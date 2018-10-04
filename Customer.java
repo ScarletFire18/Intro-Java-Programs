cfc654fd07c8eeb54a57aa58e83a101c5743be1a
ScarletFire18 <40836911+ScarletFire18@users.noreply.github.com>
2018-10-03 23:48:23 -0400

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,77 +1,40 @@
-/**
- * Customer Class
- *
- **/
-
 public class Customer extends Person
-{
-	private String idCustomer;
-	private Boolean isInMailingList;
+   {
+   private String customerNumber;
+	private Boolean mailingList;
+  
+  public Customer()
+      {
+      }
+   
+   public Customer(String n, String a, String p, String c, Boolean m)
+      {
+         super(n,a,p);
+         customerNumber = c;
+         mailingList = m;
+      }
 
+   public String getName()
+      {
+         return super.getName();
+      }
+   public String getAddress()
+      {
+         return super.getAddress();
+      }
+   public String getPhone()
+      {
+         return super.getPhone();
+      }
+   public String getCustomerNumber()
+      {
+         return this.customerNumber;
+      }
+   
+   public Boolean getMailingList()
+      {
+         return this.mailingList;
+      }
+ 
 
-	/*
-	 * The no-argument constructor of the object Customer
-	 */
-	public Customer() {
-		idCustomer = "";
-		isInMailingList = false;
-	}
-
-	/*
-	 * The constructor with parameters 
-	 * idCustomer, isMailingList
-	 */
-	public Customer(String id, Boolean isMailing) {
-		setIdCustomer(id);
-		setIsInMailingList(isMailing);
-	}
-
-	/*
-	 * This constructor initializes the object with
-	 * a name, address, phone, idCustomer, isInMailingList
-	 * @param mame
-	 * @param address
-	 * @param phone
-	 * @param idCustomer
-	 * @param isInMailingList
-	 */
-	public Customer(String name, String address, String phone, String idCustomer,
-		       	Boolean isInMailingList) {
-		
-      super.setName(name);
-      super.setAddress(address);
-      super.setPhone(phone);
-      setIdCustomer(idCustomer);
-		setIsInMailingList(isInMailingList);
-	}
-
-	/*
-	 * The setter setIdCustomer to set the field idCustomer
-	 */
-	public void setIdCustomer(String id) {
-		idCustomer = id;
-	}
-
-	/*
-	 * The getter getCustomerNumber to return the field idCustomer
-	 */
-	public String getCustomerNumber() {
-		return idCustomer;
-	}
-
-	/*
-	 * The Setter setIsInMailingList to set the field isInMailingList
-	 */
-	public void setIsInMailingList(Boolean isMailing) {
-		isInMailingList = isMailing;
-	}
-
-	/*
-	 * The Getter getIsInMailingList to return the field isInMailingList
-	 */
-	public Boolean getMailingList() {
-		return isInMailingList;
-	}
-
-}
-
+   }
